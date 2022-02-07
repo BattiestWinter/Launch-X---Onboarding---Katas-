334eaf8fe364274ddb087e155c0ea16581bc7305
BattiestWinter <42357614+BattiestWinter@users.noreply.github.com>
2022-02-07 01:54:17 -0600

    Crear y administrar proyectos
    Kata del módulo 2

---
+++ Kata_03.sh
@@ -0,0 +1,21 @@
+# Antes es necesario acceder a la carpeta elegida para conetener el proyecto
+
+# Creación del entorno virtual "env"
+python -m venv env
+
+# Activación del entorno virtual
+# Bash | Consola
+# Windows
+env\Scripts\activate.bat  
+
+# Verificación de bibliotecas instaladas
+pip freeze
+
+# Instalación de la biblioteca dateutil
+pip install python-dateutil
+
+# Verificación de bibliotecas instaladas
+pip freeze
+
+# Desactivación del entorno virtual
+deactivate
